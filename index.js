13ae4e359fe0ba566ee68b26f8eeb224a9054837
Leon Li <leon.auxinjeron@gmail.com>
2024-04-14 15:35:41 -0700

    Add bedrock dependencies

---
+++ index.js
@@ -0,0 +1,25 @@
+import * as AWS from "@aws-sdk/client-bedrock-runtime";
+const client = new AWS.BedrockRuntime({ region: "REGION" });
+
+// async/await.
+try {
+  const data = await client.invokeModel(params);
+  // process data.
+} catch (error) {
+  // error handling.
+}
+
+// Promises.
+client
+  .invokeModel(params)
+  .then((data) => {
+    // process data.
+  })
+  .catch((error) => {
+    // error handling.
+  });
+
+// callbacks.
+client.invokeModel(params, (err, data) => {
+  // process err and data.
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
-const path = require('path');
-
+// Import path for resolving file paths
+var path = require("path");
 module.exports = {
-    // Set the mode to development or production
-    mode: 'development',
-
-    // Point entry to your application's script
-    entry: './index.js',
-
-    // Define the output point
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    }
+  mode: "development",
+  // Specify the entry point for our app.
+  entry: [path.join(__dirname, "index.js")],
+  // Specify the output file containing our bundled code.
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+   // Enable WebPack to use the 'path' package.
+   resolve:{
+  fallback: { path: require.resolve("path-browserify")}
+  }
+  /**
+  * In Webpack version v2.0.0 and earlier, you must tell 
+  * webpack how to use "json-loader" to load 'json' files.
+  * To do this Enter 'npm --save-dev install json-loader' at the 
+  * command line to install the "json-loader' package, and include the 
+  * following entry in your webpack.config.js.
+  * module: {
+    rules: [{test: /\.json$/, use: use: "json-loader"}]
+  }
+  **/
 };
-

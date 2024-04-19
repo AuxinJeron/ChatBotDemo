4951b5049462c43e7355c386ac43a53f10c4be58
Leon Li <leon.auxinjeron@gmail.com>
2024-04-18 23:42:07 -0700

    Export CognitoIdentityProviderClient, InitiateAuthCommand

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import {BedrockRuntimeClient, InvokeModelCommand} from "@aws-sdk/client-bedrock-runtime";
 import {fromCognitoIdentityPool} from "@aws-sdk/credential-providers";
+import { CognitoIdentityProviderClient, InitiateAuthCommand } from '@aws-sdk/client-cognito-identity-provider';
 
-export { BedrockRuntimeClient, InvokeModelCommand, fromCognitoIdentityPool};
+export { 
+    BedrockRuntimeClient, 
+    InvokeModelCommand, 
+    fromCognitoIdentityPool,
+    CognitoIdentityProviderClient,
+    InitiateAuthCommand,
+};

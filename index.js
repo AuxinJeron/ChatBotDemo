620c2d0d8bbc18c0c68574b352ecb2902fea9cc9
Leon Li <leon.auxinjeron@gmail.com>
2024-04-22 23:50:21 -0700

    Export Ec2Client and DescribeInstancesCommand

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import {BedrockRuntimeClient, InvokeModelCommand} from "@aws-sdk/client-bedrock-runtime";
 import {fromCognitoIdentityPool} from "@aws-sdk/credential-providers";
 import { CognitoIdentityProviderClient, RespondToAuthChallengeCommand, InitiateAuthCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { EC2Client, DescribeInstancesCommand } from "@aws-sdk/client-ec2"; 
+
 
 export { 
     BedrockRuntimeClient, 
@@ -9,4 +11,6 @@ export {
     CognitoIdentityProviderClient,
     InitiateAuthCommand,
     RespondToAuthChallengeCommand,
+    EC2Client,
+    DescribeInstancesCommand
 };
